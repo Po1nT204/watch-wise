eb4c3547709f469e8d2f341d0dd5aa5f89121c46
Egor Stepanov <egor_stepanov_2004@inbox.ru>
2026-04-11 17:04:24 +0500

    feat: update logger and add export to markdown function

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,20 +1,20 @@
 import pino from 'pino';
 
-const isDev = process.env.NODE_ENV !== 'production';
+// const isDev = process.env.NODE_ENV !== 'production';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
   // В продакшене Pino пишет чистый JSON.
   // В разработке используем pino-pretty для читаемости.
-  ...(isDev && {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard', // Понятный формат времени
-        ignore: 'pid,hostname', // Убираем лишний мусор из консоли
-        sync: true, // Заставляет работать в основном потоке Next.js
-      },
-    },
-  }),
+  // ...(isDev && {
+  //   transport: {
+  //     target: 'pino-pretty',
+  //     options: {
+  //       colorize: true,
+  //       translateTime: 'SYS:standard', // Понятный формат времени
+  //       ignore: 'pid,hostname', // Убираем лишний мусор из консоли
+  //       sync: true, // Заставляет работать в основном потоке Next.js
+  //     },
+  //   },
+  // }),
 });
